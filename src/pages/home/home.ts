5d95e532fc43989d9d479b74b571f5ad605f7056
keshav joshi <keshjoshi11@gmail.com>
2017-11-23 23:52:55 +0530

    Commit 2
    Commit 2

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+    personName:string;
+  constructor(public toast: ToastController) {
+
+  }
+  showGreeting(name:string){
+    console.log(name);
+    this.toast.create({
+      message:`Hello ${name}`,
+      duration:3000
+    }).present();
+  }
+
+}
